c55e31f5ea7925c39cb208b20217a30d43b474f2
Michael Wekesa <wekesa360@outlook.com>
2022-06-02 00:00:22 +0300

    controller

---
+++ full_Server/controllers/StudentsController.js
@@ -0,0 +1,46 @@
+const readDatabase = require('../utils');
+const path = process.argv[2];
+
+
+class StudentsController {
+    static async getAllStudents(req, res) {
+        try {
+            const students = await readDatabase(path);
+            const cs = csSwe.cs;
+            const swe = csSwe.swe;
+            const hdr = 'This is the list of our students\n';
+            const c = `Number of students in CS: ${cs.length}, List: ${cs.toString().split(',').join(', ')}\n`;
+            const s = `Number of students in SWE: ${swe.length}, List: ${swe.toString().split(',').join(', ')}\n`;
+            const result = hdr + c + s;
+            res.status(200).json(result);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send('Cannot load the database');
+        }
+    }
+
+    static async getAllStudentsByMajor(req, res) {
+        try {
+          const mjr = req.params;
+          if (mjr !== 'CS' && mjr !== 'SWE') {
+            res.status(500).send('Major parameter must be CS or SWE');
+          } else {
+            const csSwe = await readDatabase(path);
+            const cs = csSwe.cs;
+            const swe = csSwe.swe;
+            if (mjr === 'CS') {
+              const out = `List: ${cs.tostring().split(',').join(', ')}`;
+              res.status(200).send(out);
+            } else {
+              const out = `List: ${swe.tostring().split(',').join(', ')}`;
+              res.status(200).send(out);
+          }
+         }
+        } catch (ex) {
+          console.log(ex);
+          res.status(500).send('Cannot load the database');
+        }
+      }
+    }
+    
+    module.exports = StudentsController;
