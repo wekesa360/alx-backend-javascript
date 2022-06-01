55d8b05a100e954fc155d05493fd120fccbfef75
Michael Wekesa <wekesa360@outlook.com>
2022-06-01 23:21:33 +0300

    task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,26 @@
+const fds = require('fs');
+
+function countStudents(path) {
+    try {
+        const csvFile = fds.readFileSync(path, 'utf-8');
+        const csvData = csvFile.split('\n');
+        const csStudent = [];
+        const sweStudent = [];
+        for (let i = 1; i < csvData.length -1; i += 1) {
+            const line = csvData[i].split(',');
+            if (line[3] === 'CS') {
+                csStudent.push(line[0].trim());
+            } else if (line[3] === 'SWE') {
+                sweStudent.push(line[0].trim());
+            }
+        }
+        const sum = csStudent.length + sweStudent.length;
+        console.log(`Number of students: ${sum}`);
+        console.log(`Number of CS students: ${csStudent.length}, List: ${csStudent.toString().split(',').join(', ')}`);
+        console.log(`Number of SWE students: ${sweStudent.length}, List: ${sweStudent.toString().split(',').join(', ')}`);
+    } catch (err) {
+        throw new Error('Cannot load the database');
+    }
+}
+
+module.exports = countStudents;
